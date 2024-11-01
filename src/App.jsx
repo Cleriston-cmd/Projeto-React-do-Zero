5c7b692afcefc5d09133d65a1de90b41f861e646
Cleriston-cmd <c.andrade6533@gmail.com>
2024-11-01 11:47:32 -0300

    Componentes de classes e funcionais

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from "react";
+
 const App = () => {
+    const [usuarios, setUsuarios] = useState(["Pablo", "José", "Manoel"])
+
+    // useEffect(() => {
+    //     setTimeout(() =>{
+    //         setUsuarios([...usuarios, "Ismael"]);
+    //     },3000)
+    // }, [])
+
     return (
         <div className="App">
             <h1>Hello DIO!</h1>
+            {usuarios.map((item) => (
+            <p>{item}</p>
+            ))}
         </div>
-    )
+    );
 }
 
 export default App;
